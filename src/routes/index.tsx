4e65fd6b5b0e1cda3fd1f6bd53348b9d4ba6fbac
Thiago Barcelos <thiagolbf@hotmail.com>
2023-02-22 22:20:10 -0300

    Header Component

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+import { HeaderComponent } from "../components/Header";
+
+export const RoutesComponent = () => {
+  return (
+    <>
+      <HeaderComponent />
+    </>
+  );
+};
