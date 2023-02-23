bfa504b6b9d91d38d1213ed64b437f0250cb5808
Thiago Barcelos <thiagolbf@hotmail.com>
2023-02-23 20:37:07 -0300

    feat add home page component

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+  font-family: var(--roboto);
+
+  > h3 {
+    text-align: justify;
+    font-weight: 300;
+  }
+`;
