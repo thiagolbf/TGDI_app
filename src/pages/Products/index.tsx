c5c4560db42461703ccbaa569945ed9ffdf07101
Thiago Barcelos <thiagolbf@hotmail.com>
2023-02-23 21:16:59 -0300

    add product card component and product page component

---
+++ src/pages/Products/index.tsx
@@ -0,0 +1,28 @@
+import { ProductsContainer } from "./style";
+
+import { ProductCardComponent } from "../../components/ProductCard";
+
+import { useContext } from "react";
+import { ProductsContext } from "../../providers/products";
+
+export const ProductsPageComponent = () => {
+  const { products } = useContext(ProductsContext);
+
+  return (
+    <>
+      <ProductsContainer>
+        {products.map((value, index) => {
+          return (
+            <ProductCardComponent
+              key={value.id}
+              image={value.foto}
+              name={value.nome}
+              description={value.descricao}
+              price={value.preco}
+            />
+          );
+        })}
+      </ProductsContainer>
+    </>
+  );
+};
